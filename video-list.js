a3c13ef7cf30521debf6b12851ba56846c83ab60
unknown <ademolababalola2003@gmail.com>
2022-11-21 11:51:02 +0100

    js: video listing and functioning

---
+++ video-list.js
@@ -0,0 +1,67 @@
+let allVideos = [
+  {
+    name: "Dont laugh challenge to its max 💀😂",
+    src: "./media/Dont laugh challenge to its max 💀😂",
+    id: "vid_1",
+  },
+  {
+    name: "HOW TO SAVE A CRASHING PLANE_!",
+    src: "./media/HOW TO SAVE A CRASHING PLANE_!",
+    id: "vid_2",
+  },
+  {
+    name: "Confirm Password using Html CSS & js",
+    src: "./media/Khaby lame and Zlatan Ibrahimovic🔥💨",
+    id: "vid_3",
+  },
+  {
+    name: "Please always survey your environment before showing people things like this 😂 (Fake Gun)",
+    src: "./media/Please always survey your environment before showing people things like this 😂 (Fake Gun)",
+    id: "vid_4",
+  },
+  {
+    name: "Sneaking a snack before dinner.",
+    src: "./media/Sneaking a snack before dinner.",
+    id: "vid_5",
+  },
+  {
+    name: "The Battle Of The Body Parts",
+    src: "./media/The Battle Of The Body Parts",
+    id: "vid_6",
+  },
+  {
+    name: "The easiest way to teach swimming! 😂",
+    src: "./media/The easiest way to teach swimming! 😂",
+    id: "vid_7",
+  },
+  {
+    name: "WHEN SHE’S HOME ALONE IN ANOTHER COUNTRY!_",
+    src: "./media/WHEN SHE’S HOME ALONE IN ANOTHER COUNTRY!_",
+    id: "vid_8",
+  },
+  {
+    name: "When your friend wants to pick up his girlfriend 😂",
+    src: "./media/When your friend wants to pick up his girlfriend 😂",
+    id: "vid_9",
+  },
+  // {
+  //   name: "How to create Login Form",
+  //   src: "./media/How to create Login Form Using Html and CSS",
+  //   id: "vid_10",
+  // },
+  // {
+  //   name: "How To Make Animated Menu Icon",
+  //   src: "./media/How To Make Animated Menu Icon Using Html",
+  //   id: "vid_11",
+  // },
+  // {
+  //   name: "How to make Read More Function",
+  //   src: "./media/How to make Read More Function using Html",
+  //   id: "vid_12",
+  // },
+  // {
+  //   name: "Vertical Navigation Bar",
+  //   src: "./media/Vertical Navigation Bar Using Html And CSS",
+  //   id: "vid_13",
+  // },
+];
